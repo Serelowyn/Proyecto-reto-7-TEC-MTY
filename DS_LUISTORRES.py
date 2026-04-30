3021a22cc680fb88e61541b807b307f2e8672655
Memo <fretn655@gmail.com>
2026-04-30 17:00:39 -0600

    se hacen unos analisis sobre la estructura del df, para poder ver tanto como sus caracteristicas, como sus columnas

--- DS_LUISTORRES.py
+++ DS_LUISTORRES.py
@@ -3,3 +3,18 @@
 import pandas as pd
 
 # ------------------- Fin de las Importaciones
+
+"""carga de archivos"""
+happiness = pd.read_csv(r"Happiness_report.csv")
+metadata = pd.read_csv(r"Metadata.csv")
+
+"""se agregan estas opciones para verificar cada uno de los dfs"""
+print(happiness.info())
+print(happiness.columns)
+print(happiness.dtypes)
+print(happiness.shape)
+
+print(metadata.info())
+print(metadata.columns)
+print(metadata.dtypes)
+print(metadata.shape)
